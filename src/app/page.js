79193d0488ad384d76ba64115c6651a076f23e57
Ameer Hamza <ameer@example.com>
2025-06-20 20:52:00 +0500

    your message

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
-export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 gap-6 bg-white text-black">
-      <h1 className="text-4xl font-bold text-center">
-        👥 Welcome to Ameer’s Social App
-      </h1>
-      <p className="text-lg text-center max-w-xl">
-        This is the start of something awesome. A social platform where friends stay connected, share goofy photos, track locations, and do much more — all built by Ameer bhai from scratch. 🚀
-      </p>
-    </div>
-  );
+  export default function Home()
+  {
+    return(
+      <div>
+        <h1> Hello bro</h1>
+      </div>
+    );  
 }
