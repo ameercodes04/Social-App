b4a90ed057364e07b8788c9b6087faedeaf4e47d
Ameer Hamza <ameer@example.com>
2025-06-20 20:58:18 +0500

    your message

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,13 @@
   export default function Home()
   {
+    const name="Ameer"
     return(
+      <main>
       <div>
-        <h1> Hello bro</h1>
+        <h1> Hello {name}!</h1>
+        <p>Welcome to your app {name}</p>
+
       </div>
+      </main>
     );  
 }
