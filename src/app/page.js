5e76f1f636c1948bef1469ffa178afef0efffece
Ameer Hamza <ameer@example.com>
2025-07-02 02:03:19 +0500

    your message

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,36 @@
-  'use client';
-  import { useState} from "react";
-  export default function Home()
+
+'use client';
+import { useState } from 'react';
+export default function Home()
+{
+  const[display, setdisplay] = useState(
+  [
+   {name: "Ali", date: "02-10-2005"},
+   {name: "Wadood", date: "03-10-2005"},
+   {name: "Hamza", date: "02-11-2005"}
+  ]
+  );
+  const[opening , setopening] = useState("Welcome to your App!");
+  const[vbutton, setbutton] = useState(true);
+  const handleClick= ()=>
   {
-    const[message, setMessage]= useState("Hello Hamza!");
-    const [buttonVisible, setButtonVisible] = useState(true);   
-    
-    const handleClick= () => {
-      setMessage("You Clicked the button fattu!"); 
-      setButtonVisible(false);
+    setopening("Birthdays Countdown: ");
+    setbutton(false);
+
   };
   return (
     <div>
-      <h1>{message}</h1>
-  
-      {buttonVisible && (
-        <button onClick={handleClick}>Click Me</button>
+      <h1>{opening}</h1>
+      <ul>               
+      {display.map((person, index) => (  
+        <li key={index}>
+          {person.name} was born on {person.date}
+        </li>
+      ))}
+    </ul>
+      {vbutton && (
+        <button onClick={handleClick}>Show Birthday Countdown</button>
       )}
     </div>
   );
-  
 }
